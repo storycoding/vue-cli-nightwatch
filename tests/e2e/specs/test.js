204a1f158fe8983b8506b38f1f0912c07acbb243
storycoding <youknownuno@gmail.com>
2018-07-27 11:28:42 +0100

    wrote first successful test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,13 +2,14 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': browser => {
+  'Demo test with localhost' : function (browser) {
     browser
-      .url(process.env.VUE_DEV_SERVER_URL)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
-      .end()
+      .url('http://localhost:8080/#/')
+      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('div[id=nav]', 1000)
+      .click('div[id=nav]')
+      .pause(1000)
+      .assert.containsText('.hello', 'Welcome to Your Vue.js App')
+      .end();
   }
-}
+};
