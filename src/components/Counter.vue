eb77e4e90dd277922cdb2606fae811c45f9b882d
storycoding <youknownuno@gmail.com>
2018-07-27 11:40:13 +0100

    Created counter component for simple test case scenario

---
+++ src/components/Counter.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<h3 data-testid='count'>{{count}}</h3>
+		<button v-on:click="increment()" data-testid='increment'>+</button>
+		<button v-on:click="decrement()" data-testid='decrement'>-</button>
+	</div>
+</template>
+
+<script>
+	
+	export default {
+		name: 'Counter',
+
+		data: function() {
+			return ({
+				count : 0
+			})
+		},
+		
+		methods: {
+			increment: function() {
+				this.count ++
+			},
+			decrement: function() {
+				if (this.count === 0) { return }
+				this.count --
+			}
+		}
+	}
+
+</script>
+
+<style scoped>
+
+	h3 {
+		color: blue;
+	}
+
+</style>
