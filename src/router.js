b81201d1cf8269c4faa9354e2d12af5c20de8f09
storycoding <youknownuno@gmail.com>
2018-07-27 11:41:10 +0100

    created routes for Counter

--- src/router.js
+++ src/router.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Counter from './components/Counter.vue'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: Home
+    },
+    {
+      path: '/counter',
+      name: 'counter',
+      component: Counter
     },
     {
       path: '/about',
