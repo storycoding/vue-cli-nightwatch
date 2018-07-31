a2d7f6db8642ea7b3228b52ccd7dee6fc67cfd42
Fábio Moreira <fabiommmoreira@gmail.com>
2018-07-31 14:43:31 +0100

    revert hello world title changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins:</h3>
+    <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank">pwa</a></li>
@@ -14,7 +14,7 @@
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank">unit-jest</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-nightwatch" target="_blank">e2e-nightwatch</a></li>
     </ul>
-    <h3>Essential Links:</h3>
+    <h3>Essential Links</h3>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -22,7 +22,7 @@
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
       <li><a href="https://news.vuejs.org" target="_blank">News</a></li>
     </ul>
-    <h3>Ecosystem:</h3>
+    <h3>Ecosystem</h3>
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank">vuex</a></li>
